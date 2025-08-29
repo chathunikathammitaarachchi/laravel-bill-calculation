111c068e64a28c8cc9ecd83ccb07e9994304baf3
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-08-29 17:55:57 +0530

    add

--- database/migrations/2025_08_15_043802_create_due_payments_table.php
+++ database/migrations/2025_08_15_043802_create_due_payments_table.php
@@ -15,6 +15,10 @@ class CreateDuePaymentsTable extends Migration
             $table->unsignedBigInteger('customer_due_id');
             $table->string('payment_method');
             $table->decimal('amount', 10, 2);
+            $table->string('cheque_number')->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('branch_name')->nullable();
+            $table->date('cheque_date')->nullable();
             $table->timestamps();
 
             $table->foreign('customer_due_id')->references('id')->on('customer_dues')->onDelete('cascade');
