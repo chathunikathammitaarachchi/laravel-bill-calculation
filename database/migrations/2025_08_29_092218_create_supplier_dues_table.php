b41e9667bce0dafd6738cd9881a00a1249147368
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-08 17:38:25 +0530

    add

--- database/migrations/2025_08_29_092218_create_supplier_dues_table.php
+++ database/migrations/2025_08_29_092218_create_supplier_dues_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
         $table->decimal('tobe_price', 10, 2);
         $table->decimal('supplier_pay', 10, 2);
         $table->decimal('balance', 10, 2);
+                $table->boolean('has_cheque_returned')->default(false);
+
             $table->timestamps();
         });
     }
