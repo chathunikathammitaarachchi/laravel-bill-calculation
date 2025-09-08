d088932b820a0e4f3059aa35604cc7292b8ff64e
chathunika dayangani thammitaarachci <chathunikathammitaarachchi74@gmail.com>
2025-09-08 17:38:13 +0530

    add

--- app/Models/SupplierDue.php
+++ app/Models/SupplierDue.php
@@ -4,16 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class SupplierDue extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['supplier_name', 'grn_no', 'g_date', 'tobe_price', 'supplier_pay', 'balance'];
 
+    // Define payments relationship
     public function payments()
     {
         return $this->hasMany(SupplierDuePayment::class, 'supplier_due_id');
     }
+
+    // Accessor to check if any cheque payment is returned
+    public function getHasChequeReturnedAttribute()
+    {
+        return $this->payments()
+                    ->where('payment_method', 'Cheque')
+                    ->where('is_returned', true)
+                    ->exists();
+    }
 }
-
-
-
-
